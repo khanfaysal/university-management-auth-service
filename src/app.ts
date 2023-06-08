9886fee3c60d54eb6fadcb047299c5690afc50f9
khan faysal <mdfaysalkhancse@gmail.com>
2023-06-08 23:23:04 +0600

    route code optimization

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,24 @@
 import express, { Application, NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
-import { UserRoutes } from './app/modules/user/user.route'
-import { AcademicSemesterRoutes } from './app/modules/academicSemester/academicSemester.route'
+import routes from './app/routes'
 // import apiError from './errors/ApiError';
 
 const app: Application = express()
 
-app.use(cors())
+app.use(cors());
+
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // application routes
-app.use('/api/v1/users',UserRoutes)
-app.use('/api/v1/academic-semester', AcademicSemesterRoutes)
-// custom throw error
+app.use('/api/v1/', routes);
+
 
 // testing purpose route
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    // console.log(x)
-    // Promise.reject((new Error('Unhandled promise rejecttion')))
   res.send('Hello World!')
-//   throw new apiError(400, 'error string ( throw apiError) property 😧')
-  // next('global error string property 😧')
 })
 
 // global error handler
